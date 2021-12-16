946d2cd7d79da1b8cd2ceb346ff5893ac89a6553
Emmanuel <emmanuelfolorunsho@icloud.com>
2021-12-16 16:25:13 +0000

    created user registration routes with JWT

--- routes/user.ts
+++ routes/user.ts
@@ -3,13 +3,24 @@ import jwt from 'jsonwebtoken'
 import bcryptjs from 'bcryptjs'
 import * as dotenv from 'dotenv'
 import { PrismaClient } from "@prisma/client";
+import cors  from 'cors'
+
 
 dotenv.config({path: '../.env'})
 
 const prisma = new PrismaClient()
 
+const allowedOrigins = ['http://localhost:3000']
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+}
+
 const router = express.Router()
 router.use(express.json())
+router.use(cors(options))
+
+
 
 router.get('/', (req:any , res:any) => {
 
@@ -17,29 +28,44 @@ router.get('/', (req:any , res:any) => {
 
 })
 
-router.post('/register', async (req: any, res: any) =>{
+interface RegistrationRequest {
+  body: {
+    email: string,
+    password: string,
+    name: string,
+    username: string
+  }
+}
 
-  const hashedPassword = await bcryptjs.hash(req.body.password, 12)
+router.post('/register', async (req: RegistrationRequest, res: any) =>{
 
-  const newUser = await prisma.user.create({
-    data: {
-      email: req.body.email,
-      password: hashedPassword,
-      name: req.body.name,
-      username: req.body.username,
-      profile: {
-        create: {
-          bio: null,
-          image: null,
-          header_image: null
+  if(req.body.email){
+      console.log(req.body.email)
+
+    const hashedPassword = await bcryptjs.hash(req.body.password, 12)
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        password: hashedPassword,
+        name: req.body.name,
+        username: req.body.username,
+        profile: {
+          create: {
+            bio: null,
+            image: null,
+            header_image: null
+          }
         }
       }
-    }
-  })
+    })
 
-  const accessToken = jwt.sign(newUser, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = jwt.sign(newUser, process.env.ACCESS_TOKEN_SECRET)
 
-  res.json({userDetails: newUser, accessToken: accessToken})
+    res.json({userDetails: newUser, accessToken: accessToken})
+  } else {
+    res.send('Error please try again')
+  }
 
 })
 
